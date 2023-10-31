6bf24659265156349f3d36da39144fcc1bb61669
Rushikesh <rushi.deshmukh2071999@gmail.com>
2023-10-31 06:38:25 +0530

    feat: error page added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/images/not-found.svg";
 
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh! page not found</h3>
+          <p>we can't seem to find the page you are looking for</p>
+          <Link to="/dashboard">back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
-    <div>
-      <h1>Error</h1>
-    </div>
+    <Wrapper>
+      <h3>Something went wrong</h3>
+    </Wrapper>
   );
 };
 
